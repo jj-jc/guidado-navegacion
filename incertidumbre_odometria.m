6bba32090e46a5dac6aabc821353bcbd29449339
german_difonzo <german_difonzo@bitbucket.org>
2020-12-03 01:52:26 +0100

    Primer commit

---
+++ incertidumbre_odometria.m
@@ -0,0 +1,73 @@
+
+% Sistema de locomoción tipo diferencial
+
+% Determinación de la incertidumbre del modelo cinemático
+
+% 1) Se obtiene la posición actual del robot
+apoloPlaceMRobot('Pioneer3AT',[-2 0 0],0);
+apoloUpdate();
+posicion_real = apoloGetLocationMRobot('Pioneer3AT');
+pos_real =[posicion_real(1),posicion_real(2),-posicion_real(4)]; % al parecer 'theta' y 'rz' tienen sentidos opuestos
+
+% 2) Se establece como offset inicial de la odometría la posición actual
+apoloResetOdometry('Pioneer3AT',pos_real);
+posicion_odometria = apoloGetOdometry('Pioneer3AT');
+
+% 3) Se mueve el robot y se mide con la odometria
+for i = 1:3000
+    apoloMoveMRobot('Pioneer3AT',[0.1, 0.1], 0.1);
+    apoloUpdate();
+    % Se almacenan en 'posicion_odometria' todos los datos de la odometría
+    pos_odometria = apoloGetOdometry('Pioneer3AT');
+    posicion_odometria = [posicion_odometria; pos_odometria]; 
+    % Se almacenan en 'pos_real' todos los datos de la posicion real del robot
+    pos_actual = apoloGetLocationMRobot('Pioneer3AT');
+    pos_real = [pos_real; pos_actual(1),pos_actual(2),pos_actual(4)];
+end
+
+% 4) Se dibuja el recorrido real y el de la odometria
+plot(posicion_odometria(:,1),posicion_odometria(:,2),'b')
+xlabel('X (m)')
+ylabel('Y (m)')
+hold on;
+plot(pos_real(:,1),pos_real(:,2),'r')
+legend('Posicion odometria','Posicion real')
+
+% 5) Se calculan las medias y varianzas de cada variable de la odometría
+error = abs(posicion_odometria - pos_real); % Matriz de errores absolutos entre odometría y posición real
+media = mean(error); % Se obtiene la media del error para cada columna 
+desviacion_estandar = std(error); % Se obtiene la desviacion estandar del error para cada columna
+var_x = (desviacion_estandar(1))^2;
+var_y = (desviacion_estandar(2))^2;
+var_theta = (desviacion_estandar(3))^2;
+
+% Se representa el error en x y en y
+figure;
+plot(error(:,1),'r')
+xlabel('Muestras')
+ylabel('Error (m)')
+hold on;
+plot(error(:,2),'b')
+legend('Error en x','Error en y')
+
+% Se representa el error en ángulo
+figure
+plot(error(:,3))
+xlabel('Muestras')
+ylabel('Error (rad)')
+
+% Se imprime por consola los datos de las medias y varianzas
+disp('---------------------------------------------------------------------------')
+disp('----------------INCERTIDUMBRE DEL MODELO CINEMÁTICO------------------------')
+disp(' ')
+disp('Valor medio de los errores absolutos cometidos con la odometría:')
+disp(['Error absoluto medio en coordenada x-------------> ', num2str(media(1)),' metros'])
+disp(['Error absoluto medio en coordenada y-------------> ', num2str(media(2)),' metros'])
+disp(['Error absoluto medio en la orientacion theta-----> ', num2str(media(3)),' rad'])
+disp(' ')
+disp('Varianza correspondiente a cada variable de la odometría:')
+disp(['Varianza de la coordenada x----------> ', num2str(var_x),' metros'])
+disp(['Varianza de la coordenada y----------> ', num2str(var_y),' metros'])
+disp(['Varianza de la orientacion theta-----> ', num2str(var_theta),' rad'])
+disp(' ')
+disp('---------------------------------------------------------------------------')
---
+++ incertidumbre_sensores.m
@@ -0,0 +1,95 @@
+
+% Sistema de percepción
+
+% Modelado de la incertidumbre de los sensores exteroceptivos
+% mediante la calibración de los mismos
+
+% Sensores utilizados: 3 ultrasonidos y 1 telémetro láser
+
+% Se coloca el robot mirando hacia la mesa principal
+apoloPlaceMRobot('Pioneer3AT',[-2 0 0],pi); 
+apoloUpdate();
+
+% 1) Calibración de los ultrasonidos
+
+% Se mide varias veces con los ultrasonidos
+dist_ultrasonidos = apoloGetAllultrasonicSensors('Pioneer3AT');
+for i = 1:1000
+    dist_ultrasonidos = [dist_ultrasonidos; apoloGetAllultrasonicSensors('Pioneer3AT')];
+    apoloUpdate();
+end
+
+% Se calcula la media de las medidas de cada ultrasonido
+media = mean(dist_ultrasonidos);
+
+% Se calcula la varianza correspondiente a la medida de cada ultrasonidos
+desviacion_std = std(dist_ultrasonidos);
+var_us_frente = (desviacion_std(1))^2;
+var_us_izq = (desviacion_std(2))^2;
+var_us_dcha = (desviacion_std(3))^2;
+
+% Se imprime por consola los datos de las medias y varianzas
+disp('-----------------------------------------------------------------------')
+disp('----------------CALIBRACION DE LOS ULTRASONIDOS------------------------')
+disp(' ')
+disp('Valor medio de las distancias medidas por los ultrasonidos:')
+disp(['Distancia media del ultrasonidos frontal-----------> ', num2str(media(1)),' metros'])
+disp(['Distancia media del ultrasonidos de la izquierda---> ', num2str(media(2)),' metros'])
+disp(['Distancia media del ultrasonidos de la derecha-----> ', num2str(media(3)),' metros'])
+disp(' ')
+disp('Varianza correspondiente a las medidas de cada ultrasonidos:')
+disp(['Varianza del ultrasonidos frontal-----------> ', num2str(var_us_frente),' metros'])
+disp(['Varianza del ultrasonidos de la izquierda---> ', num2str(var_us_izq),' metros'])
+disp(['Varianza del ultrasonidos de la derecha-----> ', num2str(var_us_dcha),' metros'])
+disp(' ')
+
+% 2) Calibración del telémetro láser
+
+% Se mide con el telémetro láser 2 balizas colocadas en el entorno
+medida_laser = apoloGetLaserLandMarks('LMS100');
+distancias = medida_laser.distance;
+angulos = medida_laser.angle;
+for i = 1:1000
+    medida_laser = apoloGetLaserLandMarks('LMS100');
+    distancias = [distancias; medida_laser.distance];
+    angulos = [angulos; medida_laser.angle];
+    apoloUpdate()
+end
+
+% Se calcula la media de las distancias y los ángulos a cada baliza
+media_dist = mean(distancias);
+media_ang = mean(angulos);
+
+% Se calcula la varianza de las distancias y los angulos a cada baliza
+
+% Distancias
+desviacion_std_dist = std(distancias);
+var_dist_LM1 = (desviacion_std_dist(1))^2;
+var_dist_LM2 = (desviacion_std_dist(2))^2;
+
+% Ángulos
+desviacion_std_ang = std(angulos);
+var_ang_LM1 = (desviacion_std_ang(1))^2;
+var_ang_LM2 = (desviacion_std_ang(2))^2;
+
+% Se imprime por consola los datos de las medias y varianzas
+disp('----------------CALIBRACION DEL TELEMETRO LASER------------------------')
+disp(' ')
+disp('Valor medio de las distancias medidas por el laser a cada baliza:')
+disp(['Distancia media a la baliza LM1-----> ', num2str(media_dist(1)),' metros'])
+disp(['Distancia media a la baliza LM2-----> ', num2str(media_dist(2)),' metros'])
+disp(' ')
+disp('Valor medio del ángulo al que se sitúan las balizas respecto el laser:')
+disp(['Angulo medio respecto a la baliza LM1----> ', num2str(media_ang(1)),' rad'])
+disp(['Ángulo medio respecto a la baliza LM2----> ', num2str(media_ang(2)),' rad'])
+disp(' ')
+disp('Varianza correspondiente a las medidas de distancias hechas por el laser:')
+disp(['Varianza de la medida de distancia a la baliza LM1-----> ', num2str(var_dist_LM1),' metros'])
+disp(['Varianza de la medida de distancia a la baliza LM2-----> ', num2str(var_dist_LM2),' metros'])
+disp(' ')
+disp('Varianza correspondiente a las medidas de ángulos hechas por el laser:')
+disp(['Varianza de la medida de ángulo a la baliza LM1-----> ', num2str(var_ang_LM1),' rad'])
+disp(['Varianza de la medida de ángulo a la baliza LM2-----> ', num2str(var_ang_LM2),' rad'])
+disp(' ')
+disp('-----------------------------------------------------------------------')
+
